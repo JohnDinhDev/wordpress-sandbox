4c60e6508db45b453a915f97d9765f9f73b8d751
John Dinh <johnduydinh@gmail.com>
2020-04-05 07:58:34 -0400

    Learn about dynamic navigation menus

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -14,23 +14,35 @@
                 <div class="site-footer__col-two">
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
-                        <ul>
-                            <li><a href="<?php echo site_url('index.php/about-us') ?>">About Us</a></li>
-                            <li><a href="#">Programs</a></li>
-                            <li><a href="#">Events</a></li>
-                            <li><a href="#">Campuses</a></li>
-                        </ul>
+
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'footerLocationTwo'
+                        ])
+                        ?>
+<!--                        <ul>-->
+<!--                            <li><a href="--><?php //echo site_url('index.php/about-us') ?><!--">About Us</a></li>-->
+<!--                            <li><a href="#">Programs</a></li>-->
+<!--                            <li><a href="#">Events</a></li>-->
+<!--                            <li><a href="#">Campuses</a></li>-->
+<!--                        </ul>-->
                     </nav>
                 </div>
 
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
-                        <ul>
-                            <li><a href="#">Legal</a></li>
-                            <li><a href="<?php echo site_url('index.php/privacy-policy'); ?>">Privacy</a></li>
-                            <li><a href="#">Careers</a></li>
-                        </ul>
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'footerLocationTwo'
+                        ])
+                        ?>
+                        <!--                        <ul>-->
+                        <!--                            <li><a href="#">Legal</a></li>-->
+                        <!--                            <li><a href="-->
+                        <?php //echo site_url('index.php/privacy-policy'); ?><!--">Privacy</a></li>-->
+                        <!--                            <li><a href="#">Careers</a></li>-->
+                        <!--                        </ul>-->
                     </nav>
                 </div>
             </div>
