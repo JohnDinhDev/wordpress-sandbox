cf6392f338299e7961eb6e4f6281c907a09cdf54
John Dinh <johnduydinh@gmail.com>
2020-04-05 09:55:39 -0400

    Reformat code & turn on debug in config

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,7 +1,6 @@
 <?php
 
-function university_files()
-{
+function university_files() {
 
     wp_enqueue_script('university_main_script', get_theme_file_uri('./js/scripts-bundled.js'), NULL, microtime(), true);
     wp_enqueue_style('roboto-condensed-font', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -12,8 +11,7 @@ function university_files()
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features()
-{
+function university_features() {
     register_nav_menu('headerMenuLocation', 'Header Menu Location');
     register_nav_menu('footerLocationOne', 'Footer Location One');
     register_nav_menu('footerLocationTwo', 'Footer Location Two');
--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -23,13 +23,14 @@
                     'theme_location' => 'headerMenuLocation'
                 ]);
                 ?>
-<!--                <ul>-->
-<!--                    <li><a href="--><?php //echo site_url('index.php/about-us') ?><!--">About Us</a></li>-->
-<!--                    <li><a href="#">Programs</a></li>-->
-<!--                    <li><a href="#">Events</a></li>-->
-<!--                    <li><a href="#">Campuses</a></li>-->
-<!--                    <li><a href="#">Blog</a></li>-->
-<!--                </ul>-->
+                <!--                <ul>-->
+                <!--                    <li><a href="-->
+                <?php //echo site_url('index.php/about-us') ?><!--">About Us</a></li>-->
+                <!--                    <li><a href="#">Programs</a></li>-->
+                <!--                    <li><a href="#">Events</a></li>-->
+                <!--                    <li><a href="#">Campuses</a></li>-->
+                <!--                    <li><a href="#">Blog</a></li>-->
+                <!--                </ul>-->
             </nav>
             <div class="site-header__util">
                 <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
