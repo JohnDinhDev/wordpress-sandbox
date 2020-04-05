997291acec99a7a2c96d2870e92f733409800d5b
John Dinh <johnduydinh@gmail.com>
2020-04-05 11:22:25 -0400

    Learn about pagination with WP

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -32,7 +32,9 @@ get_header();
                     <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
                 </div>
             </div>
-        <?php } ?>
+        <?php }
+        echo paginate_links()
+        ?>
     </div>
 <?php
 get_footer();
